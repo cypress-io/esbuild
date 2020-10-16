9db398626bfeacb6befadf76c6dc9619c0f8fff1
Thorsten Lorenz <thlorenz@gmx.de>
2020-10-16 16:37:21 -0500

    snap: passing electron-link function wrapper tests

--- internal/snap_printer/snap_printer_electron_link_test.go
+++ internal/snap_printer/snap_printer_electron_link_test.go
@@ -23,3 +23,157 @@ function main() {
 `,
 		func(mod string) bool { return mod == "a" })
 }
+
+//
+// Function Closures
+//
+
+// First three following are parts of the related electron-link example which is
+// tested in one piece in the forth test
+// test('requires that appear in a closure wrapper defined in the top-level scope (e.g. CoffeeScript)')
+func TestElinkTopLevelClosureWrapperCall(t *testing.T) {
+	expectPrinted(t, `
+(function () {
+	const a = require('a')
+	const b = require('b')
+	function main () {
+		return a + b
+	}
+}).call(this)
+`, `
+(function() {
+
+let a;
+function __get_a__() {
+  return a = a || require("a")
+}
+
+let b;
+function __get_b__() {
+  return b = b || require("b")
+}
+  function main() {
+    return __get_a__() + __get_b__();
+  }
+}).call(this);
+`, ReplaceAll)
+}
+
+func TestElinkTopLevelClosureWrapperSelfExecuteFiltered(t *testing.T) {
+	expectPrinted(t, `
+(function () {
+  const a = require('a')
+  const b = require('b')
+  function main () {
+    return a + b
+  }
+})()
+`, `
+(function() {
+
+let a;
+function __get_a__() {
+  return a = a || require("a")
+}
+  const b = require("b");
+  function main() {
+    return __get_a__() + b;
+  }
+})();
+`,
+		func(mod string) bool { return mod == "a" },
+	)
+}
+
+// NOTE: electron-link does not rewrite anything here, however this may be a mistake as
+// `foo` might invoke the callback synchronously when it runs and thus execute the `require`s
+func TestElinkTopLevelFunctionInvokingCallback(t *testing.T) {
+	expectPrinted(t, `
+foo(function () {
+  const b = require('b')
+  const c = require('c')
+  function main () {
+    return b + c
+  }
+})
+`, `
+foo(function() {
+
+let b;
+function __get_b__() {
+  return b = b || require("b")
+}
+
+let c;
+function __get_c__() {
+  return c = c || require("c")
+}
+  function main() {
+    return __get_b__() + __get_c__();
+  }
+});
+`,
+		ReplaceAll,
+	)
+}
+func TestElinkTopLevelClosureCompleteFiltered(t *testing.T) {
+	expectPrinted(t, `
+(function () {
+  const a = require('a')
+  const b = require('b')
+  function main () {
+    return a + b
+  }
+}).call(this)
+
+(function () {
+  const a = require('a')
+  const b = require('b')
+  function main () {
+    return a + b
+  }
+})()
+
+foo(function () {
+  const b = require('b')
+  const c = require('c')
+  function main () {
+    return b + c
+  }
+})
+`, `
+(function() {
+
+let a;
+function __get_a__() {
+  return a = a || require("a")
+}
+  const b = require("b");
+  function main() {
+    return __get_a__() + b;
+  }
+}).call(this)(function() {
+
+let a;
+function __get_a__() {
+  return a = a || require("a")
+}
+  const b = require("b");
+  function main() {
+    return __get_a__() + b;
+  }
+})();
+foo(function() {
+  const b = require("b");
+
+let c;
+function __get_c__() {
+  return c = c || require("c")
+}
+  function main() {
+    return b + __get_c__();
+  }
+});
+`,
+		func(mod string) bool { return mod == "a" || mod == "c" })
+}
--- internal/snap_printer/snap_test_utils.go
+++ internal/snap_printer/snap_test_utils.go
@@ -12,35 +12,6 @@ import (
 	"testing"
 )
 
-func RunOnly(
-	contents string,
-) {
-	options := PrintOptions{}
-	log := logger.NewDeferLog()
-
-	tree, ok := js_parser.Parse(log, test.SourceForTest(contents), config.Options{
-		UnsupportedJSFeatures: options.UnsupportedFeatures,
-	})
-	msgs := log.Done()
-	text := ""
-	for _, msg := range msgs {
-		text += msg.String(logger.StderrOptions{}, logger.TerminalInfo{})
-	}
-	if len(text) > 0 {
-		fmt.Printf(text)
-		panic("Parse error")
-	}
-	if !ok {
-		panic("Parse error")
-	}
-	symbols := js_ast.NewSymbolMap(1)
-	symbols.Outer[0] = tree.Symbols
-	r := snap_renamer.NewSnapRenamer(symbols)
-	var js []byte
-	js = Print(tree, symbols, r, options, ReplaceAll).JS
-	fmt.Println(strings.TrimSpace(string(js)))
-}
-
 func assertEqual(t *testing.T, a interface{}, b interface{}) {
 	t.Helper()
 	if a != b {
@@ -48,7 +19,7 @@ func assertEqual(t *testing.T, a interface{}, b interface{}) {
 	}
 }
 
-type TestOpts struct {
+type testOpts struct {
 	shouldReplaceRequire func(string) bool
 	compareByLine        bool
 	debug                bool
@@ -64,7 +35,7 @@ func expectPrintedCommon(
 	contents string,
 	expected string,
 	options PrintOptions,
-	testOpts TestOpts,
+	testOpts testOpts,
 ) {
 	t.Helper()
 	t.Run(name, func(t *testing.T) {
@@ -101,7 +72,11 @@ func expectPrintedCommon(
 			}
 
 		} else {
-			assertEqual(t, actualTrimmed, expectedTrimmed)
+			if testOpts.debug {
+				fmt.Println(actualTrimmed)
+			} else {
+				assertEqual(t, actualTrimmed, expectedTrimmed)
+			}
 		}
 	})
 }
@@ -114,7 +89,7 @@ func expectPrinted(t *testing.T, contents string, expected string, shouldReplace
 		contents,
 		expected,
 		PrintOptions{},
-		TestOpts{shouldReplaceRequire, false, false},
+		testOpts{shouldReplaceRequire, false, false},
 	)
 }
 
@@ -126,7 +101,7 @@ func expectByLine(t *testing.T, contents string, expected string, shouldReplaceR
 		contents,
 		expected,
 		PrintOptions{},
-		TestOpts{shouldReplaceRequire, true, false},
+		testOpts{shouldReplaceRequire, true, false},
 	)
 }
 
@@ -138,7 +113,19 @@ func debugByLine(t *testing.T, contents string, expected string, shouldReplaceRe
 		contents,
 		expected,
 		PrintOptions{},
-		TestOpts{shouldReplaceRequire, true, true},
+		testOpts{shouldReplaceRequire, true, true},
+	)
+}
+
+func debugPrinted(t *testing.T, contents string, expected string, shouldReplaceRequire func(string) bool) {
+	t.Helper()
+	expectPrintedCommon(
+		t,
+		contents,
+		contents,
+		expected,
+		PrintOptions{},
+		testOpts{shouldReplaceRequire, false, true},
 	)
 }
 
