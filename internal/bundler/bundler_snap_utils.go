b021b1bb45cb875aade992997ce0281d8cc1aab2
Thorsten Lorenz <thlorenz@gmx.de>
2020-11-09 16:39:40 -0500

    snap_api: including entrypoint info in meta

--- internal/bundler/bundler_snap_utils.go
+++ internal/bundler/bundler_snap_utils.go
@@ -11,9 +11,11 @@ func fileInfoJSON(f *file, r renamer.Renamer) string {
 	return fmt.Sprintf(`{
              "identifierName": "%s",
              "fullPath": "%s",
+             "isEntryPoint": %t,
              "replacementFunction": "%s"
            }`,
 		f.source.IdentifierName,
 		f.source.KeyPath.Text,
+		f.isEntryPoint,
 		replacementFunc)
 }
