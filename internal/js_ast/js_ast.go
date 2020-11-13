3eeb2c46697025199fa534dc0572811efc402179
Thorsten Lorenz <thlorenz@gmx.de>
2020-11-13 15:10:00 -0500

    core: js_ast adding refs needed to snapshot

--- internal/js_ast/js_ast.go
+++ internal/js_ast/js_ast.go
@@ -1358,6 +1358,8 @@ type AST struct {
 	HasTopLevelReturn bool
 	UsesExportsRef    bool
 	UsesModuleRef     bool
+	UsesDirnameRef    bool
+	UsesFilenameRef   bool
 
 	// This is a list of ES6 features
 	HasES6Imports bool
@@ -1372,7 +1374,10 @@ type AST struct {
 	CharFreq    *CharFreq
 	ExportsRef  Ref
 	ModuleRef   Ref
+	RequireRef  Ref
 	WrapperRef  Ref
+	DirnameRef  Ref
+	FilenameRef Ref
 
 	// These are stored at the AST level instead of on individual AST nodes so
 	// they can be manipulated efficiently without a full AST traversal
