0f70c7eaff1e40f9a33fa4adafa51ed42e551bc8
Thorsten Lorenz <thlorenz10@gmail.com>
2021-09-06 14:25:16 -0500

    snap: api normalizes module path to determine replace require (#7)
    * snap: api normalizes module path to determine replace require
    
    * chore: update snapbuild npm versions

--- internal/snap_api/snap_api.go
+++ internal/snap_api/snap_api.go
@@ -1,9 +1,10 @@
 package snap_api
 
 import (
+	"strings"
+
 	"github.com/evanw/esbuild/internal/resolver"
 	"github.com/evanw/esbuild/pkg/api"
-	"strings"
 )
 
 func IsExternalModule(platform api.Platform, external []string) api.ShouldReplaceRequirePredicate {
@@ -34,6 +35,11 @@ func CreateShouldReplaceRequire(
 ) api.ShouldReplaceRequirePredicate {
 	isExternal := IsExternalModule(platform, external)
 	return func(mdl string) bool {
+		// NOTE: normalizing cache/require keys to always use forward slashes
+		// We could already store them as such which would be more efficient, but after measuring
+		// I found zero perf overhead on OSX. Nothing gets replaced in this case so the only affected
+		// OS would be windows, but should be neglible there as well.
+		mdl = strings.ReplaceAll(mdl, "\\", "/")
 		return isExternal(mdl) || IsNative(mdl) || replaceRequire(mdl) || !rewriteModule(mdl)
 	}
 }
